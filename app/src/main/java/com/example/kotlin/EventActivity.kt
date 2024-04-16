07137bfa7ef3af9e2b4fdeb43e2a0b8c63060714
914JugariuDiana <dia.jugariu4@gmail.com>
2024-04-16 11:57:47 +0300

    Advertising while scanning added.

--- app/src/main/java/com/example/kotlin/EventActivity.kt
+++ app/src/main/java/com/example/kotlin/EventActivity.kt
@@ -1,9 +1,21 @@
 package com.example.kotlin
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.AdvertiseData
+import android.bluetooth.le.AdvertisingSet
+import android.bluetooth.le.AdvertisingSetCallback
+import android.bluetooth.le.AdvertisingSetParameters
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.ParcelUuid
+import android.provider.Settings
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -23,8 +35,15 @@ import com.example.kotlin.ui.theme.KotlinTheme
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import java.util.UUID
 
 class EventActivity : ComponentActivity() {
+    companion object {
+        private const val REQUEST_BLUETOOTH_PERMISSION = 123
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val requestPermissions = RequestPermissions(context = this@EventActivity, activity = this)
@@ -38,11 +57,83 @@ class EventActivity : ComponentActivity() {
                     val bleScanner = BleScanner(context = this@EventActivity, requestPermissions)
                     bleScanner.scan()
                     Surface(color = MaterialTheme.colorScheme.background) {
+                        advertise()
                         show(bleScanner.getLeDeviceListAdapter())
                     }
                 }
             }
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun advertise(){
+        val advertiser = BluetoothAdapter.getDefaultAdapter().bluetoothLeAdvertiser
+
+        val parameters = AdvertisingSetParameters.Builder()
+            .setLegacyMode(true) // True by default, but set here as a reminder.
+            .setConnectable(false)
+            .setScannable(true)
+            .setInterval(AdvertisingSetParameters.INTERVAL_HIGH)
+            .setTxPowerLevel(AdvertisingSetParameters.TX_POWER_MEDIUM)
+            .build()
+
+
+        val deviceId = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID);
+        val data = AdvertiseData.Builder()
+            .setIncludeDeviceName(true)
+//                .addTransportDiscoveryData(TransportDiscoveryData("143252".toByteArray()))
+            .addManufacturerData(15, "15823".toByteArray(Charsets.UTF_8))
+            .build()
+
+        lateinit var currentAdvertisingSet: AdvertisingSet
+
+        val callback: AdvertisingSetCallback = object : AdvertisingSetCallback() {
+            override fun onAdvertisingSetStarted(
+                advertisingSet: AdvertisingSet,
+                txPower: Int,
+                status: Int
+            ) {
+                Log.i(
+                    LOG_TAG, "onAdvertisingSetStarted(): txPower:" + txPower + " , status: "
+                            + status
+                )
+                currentAdvertisingSet = advertisingSet
+                if (ActivityCompat.checkSelfPermission(
+                        this@EventActivity,
+                        Manifest.permission.BLUETOOTH_ADVERTISE
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+                currentAdvertisingSet.setAdvertisingData(
+                    AdvertiseData.Builder().setIncludeDeviceName(true).setIncludeTxPowerLevel(true).build()
+                )
+                currentAdvertisingSet.setScanResponseData(
+                    AdvertiseData.Builder().addServiceUuid(ParcelUuid(UUID.randomUUID())).build()
+                )
+            }
+
+            override fun onAdvertisingDataSet(advertisingSet: AdvertisingSet, status: Int) {
+                Log.i(LOG_TAG, "onAdvertisingDataSet() :status:$status")
+            }
+
+            override fun onScanResponseDataSet(advertisingSet: AdvertisingSet, status: Int) {
+                Log.i(LOG_TAG, "onScanResponseDataSet(): status:$status")
+            }
+
+            override fun onAdvertisingSetStopped(advertisingSet: AdvertisingSet) {
+                Log.i(LOG_TAG, "onAdvertisingSetStopped():")
+            }
+        }
+
+        advertiser.startAdvertisingSet(parameters, data, null, null, null, callback)
     }
 }
 
